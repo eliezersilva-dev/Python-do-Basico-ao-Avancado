0e9109c2fcae89add1727d861bc844bd0889497a
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-09-08 23:16:40 -0300

    addAulas

---
+++ 3-Programação Orientada à Objetos (POO)/aula014/main.py
@@ -0,0 +1,17 @@
+"""
+Criando Exceções
+"""
+
+
+class TaErradoError(Exception):
+    pass
+
+
+def testar():
+    raise TaErradoError('Errado')
+
+
+try:
+    testar()
+except TaErradoError as error:
+    print(f'Erro: {error}')
