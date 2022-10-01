991f3f3d014f12a198f100fd6c4df6853d812f46
Eliezer Silva <eliezersilva.dev@gmail.com>
2022-09-30 22:00:10 -0300

    aulaNameMain

---
+++ 4-Módulos Python/aula_001/app.py
@@ -0,0 +1,6 @@
+from modulo import soma
+
+print(soma(30, 40))
+
+print(soma(2, 2))
+print(soma(3, 5))
---
+++ 4-Módulos Python/aula_001/modulo.py
@@ -0,0 +1,10 @@
+
+def soma(x: float, y: float) -> float:
+    return x + y
+
+
+print(__name__)
+
+if __name__ == '__main__':
+    print(soma(2, 2))
+    print(soma(3, 5))
